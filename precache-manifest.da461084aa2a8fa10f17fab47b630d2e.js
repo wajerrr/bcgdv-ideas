43e2af4814d2f7aafe9bdca064a8621728ee2730
PW <pw@PWs-MacBook-Pro.local>
2018-11-14 12:21:18 +0000

    Updates

---
+++ precache-manifest.da461084aa2a8fa10f17fab47b630d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e0b86f18cc8e80276f7",
+    "url": "/bcgdv-ideas/static/css/main.9861ff09.chunk.css"
+  },
+  {
+    "revision": "5e0b86f18cc8e80276f7",
+    "url": "/bcgdv-ideas/static/js/main.5e0b86f1.chunk.js"
+  },
+  {
+    "revision": "489460414d9d49933561",
+    "url": "/bcgdv-ideas/static/js/1.48946041.chunk.js"
+  },
+  {
+    "revision": "2d2e80bc5f3750f76201",
+    "url": "/bcgdv-ideas/static/js/runtime~main.2d2e80bc.js"
+  },
+  {
+    "revision": "8da9661d47af840fa3010893309864fe",
+    "url": "/bcgdv-ideas/index.html"
+  }
+];
